bdc76614fd52af2808ebe747469e61664f77c56e
Jan Leirvaag <lja022025@gmail.com>
2025-08-22 11:41:44 +0200

    update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,6 +11,11 @@ namespace HHG_WPF_Fileversion
         {
         //adding player field so we can instantiate player object in MainWindow's constructor
         private Player player;
+
+        private ScaleTransform zoomTransform = new ScaleTransform(1, 1);
+        private RotateTransform rotateTransform = new RotateTransform(0);
+        private TransformGroup transformGroup = new TransformGroup();
+
         public MainWindow()
             {
             InitializeComponent();
@@ -24,18 +29,41 @@ namespace HHG_WPF_Fileversion
 
         private void FadeInImage()
             {
-            DoubleAnimation fadeIn = new DoubleAnimation(0, 1, TimeSpan.FromSeconds(5));
+            DoubleAnimation fadeIn = new DoubleAnimation(0, 0.25, TimeSpan.FromSeconds(5));
             image.BeginAnimation(UIElement.OpacityProperty, fadeIn);
+            }
+
+        private void ZoomIn()
+            {
+            //ScaleTransform zoomTransform = new ScaleTransform(1, 1);
+
+            //image.RenderTransformOrigin = new Point(0, 0);
+
+            //image.RenderTransform = zoomTransform;
+
+            DoubleAnimation zoomIn = new DoubleAnimation
+                {
+                From = 1.0,
+                To = 1.5,
+                Duration = TimeSpan.FromSeconds(2),
+                AutoReverse = false,
+                //RepeatBehavior = RepeatBehavior.Forever
+                };
+
+            zoomTransform.BeginAnimation(ScaleTransform.ScaleXProperty, zoomIn);
+            zoomTransform.BeginAnimation(ScaleTransform.ScaleYProperty, zoomIn);
             }
 
         private void StartImageSpin()
             {
             // Create a RotateTransform and set it on the image
-            RotateTransform rotateTransform = new RotateTransform();
-            image.RenderTransform = rotateTransform;
+            //RotateTransform rotateTransform = new RotateTransform();
+            //image.RenderTransform = rotateTransform;
+
+
 
             // Set the rotation center to the center of the image
-            image.RenderTransformOrigin = new Point(0.5, 0.5); // Center
+            //image.RenderTransformOrigin = new Point(0.5, 0.5); // Center
 
             // Create the animation
             DoubleAnimation rotateAnimation = new DoubleAnimation
@@ -49,12 +77,19 @@ namespace HHG_WPF_Fileversion
 
             // Apply the animation to the RotateTransform
             rotateTransform.BeginAnimation(RotateTransform.AngleProperty, rotateAnimation);
+
+            //zoomTransform.BeginAnimation(RotateTransform.AngleProperty, rotateAnimation);
             }
 
         private void btnOK_Click(object sender, RoutedEventArgs e)
             {
             bool missingInfo = false;
             const string warning = "Please fill out all fields!";
+
+            image.RenderTransformOrigin = new Point(0.5, 0.5);
+            transformGroup.Children.Add(zoomTransform);
+            transformGroup.Children.Add(rotateTransform);
+            image.RenderTransform = transformGroup;
 
             image.Visibility = Visibility.Hidden;
             player.ClearPlayerData(player);
@@ -68,16 +103,19 @@ namespace HHG_WPF_Fileversion
 
                 tbQuote.Text = warning;
                 image.Visibility = Visibility.Visible;
-                image.Opacity = 0.25;
+                //image.Opacity = 0.25;
                 image.Source = player.ShowImage(player, missingInfo);
+
                 FadeInImage();
+                ZoomIn();
+
                 StartImageSpin();
                 }
             else
             if (player.Age == player.dontPanic)
                 {
                 image.Visibility = Visibility.Visible;
-                image.Opacity = 1.0;
+                //image.Opacity = 1.0;
 
                 image.Source = player.ShowImage(player, missingInfo);
                 FadeInImage();
