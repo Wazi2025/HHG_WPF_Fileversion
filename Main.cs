75139d9f87d759bf64346fb0a365aa94e52a6c30
Jan Leirvaag <lja022025@gmail.com>
2025-09-04 11:12:52 +0200

    Added Music and Gfx manager classes since Player was starting to become rather bureaucratic

--- Main.cs
+++ Main.cs
@@ -4,11 +4,156 @@ using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 
 
 namespace HHG_WPF_Fileversion
     {
+    public class MusicManager
+        {
+        //we'll use NAudio since MediaPlayer class has limititations
+        public IWavePlayer outputDevice;
+        public AudioFileReader audioFileReader;
+        public FadeInOutSampleProvider fade;
+
+        public void InitMusicStuff(Player player)
+            {
+            //init audio stuff
+            audioFileReader = new AudioFileReader(GetSong(player));
+            fade = new FadeInOutSampleProvider(audioFileReader, true);
+
+            //set fade-in to 2 seconds
+            fade.BeginFadeIn(2000);
+
+            //attach to an output device
+            outputDevice = new WaveOutEvent();
+            outputDevice.Init(fade);
+
+            //start playing
+            outputDevice.Play();
+            }
+
+        public string GetSong(Player player)
+            {
+            player.fileName = "Journey of the Sorcerer.mp4";
+            player.filePath = Path.Combine(player.projectRoot, player.fileDir, player.fileName);
+
+            return player.filePath;
+            }
+        }//end of MusicManager class
+
+    public class GfxManager
+        {
+        //declare and initialize a BitmapImage for use with...well, bitmaps AKA images
+        private BitmapImage bitmapImage = new BitmapImage();
+
+        //declare and initialize animation stuff
+        private ScaleTransform zoomTransform = new ScaleTransform(1, 1);
+        private RotateTransform rotateTransform = new RotateTransform(0);
+        private TransformGroup transformGroup = new TransformGroup();
+
+        public void InitScreenStuff(Player player, GfxManager gfxManager, Window mainWindow)
+            {
+            //create new brush and set window's background image
+            ImageBrush brush = new ImageBrush();
+            brush.Opacity = 0.25;
+            brush.ImageSource = gfxManager.ShowImage(player);
+            mainWindow.Background = brush;
+
+            mainWindow.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            mainWindow.WindowState = WindowState.Maximized;
+            }
+
+        public void FadeInImage(double maxOpacity, Image image)
+            {
+            DoubleAnimation fadeIn = new DoubleAnimation(0, maxOpacity, TimeSpan.FromSeconds(5));
+
+            fadeIn.AutoReverse = true;
+            fadeIn.RepeatBehavior = RepeatBehavior.Forever;
+            image.BeginAnimation(UIElement.OpacityProperty, fadeIn);
+            }
+
+        public void ZoomIn(bool missingInfo, Player player)
+            {
+            //the spinning animation will activate even though it's never called
+            //when the spin animation starts it effectively carries over due to the use of TransFormGroup
+            //the solution is to set it to null (remove it)
+            rotateTransform.BeginAnimation(RotateTransform.AngleProperty, null);
+
+            DoubleAnimation zoomIn = new DoubleAnimation();
+
+            zoomIn.From = 0.0;
+            zoomIn.To = 3.5;
+            zoomIn.Duration = TimeSpan.FromSeconds(2);
+            zoomIn.AutoReverse = true;
+            zoomIn.RepeatBehavior = RepeatBehavior.Forever;
+
+            //only bounce if only 42 
+            if (missingInfo && player.Age == player.DontPanic)
+                {
+                BounceEase bounceEase = new BounceEase();
+                bounceEase.Bounces = 1;
+                bounceEase.Bounciness = 4;
+                bounceEase.EasingMode = EasingMode.EaseOut;
+
+                zoomIn.EasingFunction = bounceEase;
+                }
+
+            //apply the animation (and bounce if applicable) to zoomTransform
+            zoomTransform.BeginAnimation(ScaleTransform.ScaleXProperty, zoomIn);
+            zoomTransform.BeginAnimation(ScaleTransform.ScaleYProperty, zoomIn);
+            }
+
+        public void StartImageSpin()
+            {
+            DoubleAnimation rotateAnimation = new DoubleAnimation();
+
+            rotateAnimation.From = 0;
+            rotateAnimation.To = 360;
+            rotateAnimation.Duration = TimeSpan.FromSeconds(10);
+            rotateAnimation.RepeatBehavior = RepeatBehavior.Forever;
+
+            //apply the animation (spin) to rotateTransform
+            rotateTransform.BeginAnimation(RotateTransform.AngleProperty, rotateAnimation);
+            }
+
+        public void InitImageControl(Image image)
+            {
+            //only one animation can be active so we have to add both animations to a TransformGroup
+            //and then add the TransformGroup to the image RenderTransform
+            image.RenderTransformOrigin = new Point(0.5, 0.5);
+            transformGroup.Children.Add(zoomTransform);
+            transformGroup.Children.Add(rotateTransform);
+            image.RenderTransform = transformGroup;
+            }
+
+        public BitmapImage ShowImage(Player player)
+            {
+            player.fileName = "Andromeda-Galaxy-Milky-Way.jpg";
+            player.filePath = Path.Combine(player.projectRoot, player.fileDir, player.fileName);
+
+            bitmapImage = new BitmapImage(new Uri(player.filePath));
+
+            return bitmapImage;
+            }
+
+        public BitmapImage ShowImage(bool missingInfo, Player player)
+            {
+            if (missingInfo)
+                player.fileName = "hhg2.png";
+            else
+                player.fileName = "DontPanic.jpg";
+
+            player.filePath = Path.Combine(player.projectRoot, player.fileDir, player.fileName);
+
+            bitmapImage = new BitmapImage(new Uri(player.filePath));
+
+            return bitmapImage;
+            }
+        }//end of GfxManager
+
     public class Player
         {
         //We only need get/set when property is to be accessed/modified outside it's class, i.e. it's public
@@ -25,21 +170,16 @@ namespace HHG_WPF_Fileversion
         //declare and initialize string list to store quotes from file
         private List<string> greetingList = new List<string>();
 
-        //declare and initialize a BitmapImage for use with...well, bitmaps AKA images
-        private BitmapImage bitmapImage = new BitmapImage();
+        ////declare and initialize a BitmapImage for use with...well, bitmaps AKA images
+        //private BitmapImage bitmapImage = new BitmapImage();
 
-        private readonly string projectRoot = Directory.GetParent(AppContext.BaseDirectory).Parent.Parent.Parent.FullName;
-        private readonly string fileDir = "Data";
-        private string fileName = "";
-        private string filePath = "";
+        public string projectRoot { get; set; } = Directory.GetParent(AppContext.BaseDirectory).Parent.Parent.Parent.FullName;
+        public string fileDir { get; set; } = "Data";
+        public string fileName { get; set; } = "";
+        public string filePath { get; set; } = "";
 
         public readonly string warning = "'Please fill out all fields. Although bypasses are the bedrock of humanity, this is the one and only exception.'";
         public readonly string author = "\n - Prostetnic Vogon Jeltz -";
-
-        //we'll use NAudio since MediaPlayer class has limititations
-        public IWavePlayer outputDevice;
-        public AudioFileReader audioFileReader;
-        public FadeInOutSampleProvider fade;
 
         //Player constructor
         //public Player()
@@ -68,37 +208,6 @@ namespace HHG_WPF_Fileversion
                 }
             }// end of ReadFromFile method
 
-        public string GetSong()
-            {
-            fileName = "Journey of the Sorcerer.mp4";
-            filePath = Path.Combine(projectRoot, fileDir, fileName);
-
-            return filePath;
-            }
-
-        public BitmapImage ShowImage()
-            {
-            fileName = "Andromeda-Galaxy-Milky-Way.jpg";
-            filePath = Path.Combine(projectRoot, fileDir, fileName);
-
-            bitmapImage = new BitmapImage(new Uri(filePath));
-
-            return bitmapImage;
-            }
-
-        public BitmapImage ShowImage(bool missingInfo)
-            {
-            if (missingInfo)
-                fileName = "hhg2.png";
-            else
-                fileName = "DontPanic.jpg";
-
-            filePath = Path.Combine(projectRoot, fileDir, fileName);
-
-            bitmapImage = new BitmapImage(new Uri(filePath));
-
-            return bitmapImage;
-            }
 
         public void ClearPlayerData(Player player)
             {
@@ -135,15 +244,15 @@ namespace HHG_WPF_Fileversion
 
             }
 
-        public void ReadInput(string FirstName, string LastName, string age, Player player)
+        public void ReadInput(string firstName, string lastName, string age, Player player)
             {
             //ask the user for their FirstName, LastName and Age and add these values to their respective player properties
-            player.FirstName = FirstName;
-            player.LastName = LastName;
+            FirstName = firstName;
+            LastName = lastName;
 
             if (int.TryParse(age, out int result))
-                player.Age = result;
+                Age = result;
 
-            }//end of ReadInput
+            }
         }//end of class Player
     }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,25 +1,23 @@
-﻿using NAudio.Wave;
-using NAudio.Wave.SampleProviders;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
 using System.Windows.Media;
-using System.Windows.Media.Animation;
 
 namespace HHG_WPF_Fileversion
-{
+    {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
+        {
         //adding player field so we can instantiate player object in MainWindow's constructor
         private Player player;
 
-        //declare and initialize animation stuff
-        private ScaleTransform zoomTransform = new ScaleTransform(1, 1);
-        private RotateTransform rotateTransform = new RotateTransform(0);
-        private TransformGroup transformGroup = new TransformGroup();
+        //adding musicManager field
+        private MusicManager musicManager;
+
+        //adding gfxManager field
+        private GfxManager gfxManager;
 
         //declare and initialize a Random object
         private Random random = new Random();
@@ -32,23 +30,29 @@ namespace HHG_WPF_Fileversion
 
         //MainWindow's constructor
         public MainWindow()
-        {
+            {
             InitializeComponent();
 
             //Instantiate player object and pass it as a parameter whenever we need to access it outside this (MainWindow) class
             player = new Player();
 
+            //we'll do the same here as with player
+            musicManager = new MusicManager();
+
+            //we'll do the same here as with player
+            gfxManager = new GfxManager();
+
             //fetch contents in quotes file
             player.ReadFromFile(player);
 
             //init screen stuff
-            InitScreenStuff();
+            gfxManager.InitScreenStuff(player, gfxManager, mainWindow);
 
             //init animation stuff
-            InitImageControl();
+            gfxManager.InitImageControl(image);
 
             //init music stuff
-            InitMusicStuff();
+            musicManager.InitMusicStuff(player);
 
             //set focus to FirstName textbox
             //note: prolly set this in MainWindow.xaml
@@ -57,102 +61,10 @@ namespace HHG_WPF_Fileversion
             //set textwrap on
             //don't need this, done in MainWindow.xaml
             //tbQuote.TextWrapping = TextWrapping.Wrap;
-        }
-
-        private void InitMusicStuff()
-        {
-            //init audio stuff
-            player.audioFileReader = new AudioFileReader(player.GetSong());
-            player.fade = new FadeInOutSampleProvider(player.audioFileReader, true);
-
-            //set fade-in to 2 seconds
-            player.fade.BeginFadeIn(2000);
-
-            //attach to an output device
-            player.outputDevice = new WaveOutEvent();
-            player.outputDevice.Init(player.fade);
-
-            //start playing
-            player.outputDevice.Play();
-        }
-
-        private void InitScreenStuff()
-        {
-            //create new brush and set window's background image
-            ImageBrush brush = new ImageBrush();
-            brush.Opacity = 0.25;
-            brush.ImageSource = player.ShowImage();
-            this.Background = brush;
-
-            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
-            this.WindowState = WindowState.Maximized;
-        }
-
-        private void FadeInImage(double maxOpacity)
-        {
-            DoubleAnimation fadeIn = new DoubleAnimation(0, maxOpacity, TimeSpan.FromSeconds(5));
-
-            fadeIn.AutoReverse = true;
-            fadeIn.RepeatBehavior = RepeatBehavior.Forever;
-            image.BeginAnimation(UIElement.OpacityProperty, fadeIn);
-        }
-
-        private void ZoomIn(bool missingInfo)
-        {
-            //the spinning animation will activate even though it's never called
-            //when the spin animation starts it effectively carries over due to the use of TransFormGroup
-            //the solution is to set it to null (remove it)
-            rotateTransform.BeginAnimation(RotateTransform.AngleProperty, null);
-
-            DoubleAnimation zoomIn = new DoubleAnimation();
-
-            zoomIn.From = 0.0;
-            zoomIn.To = 3.5;
-            zoomIn.Duration = TimeSpan.FromSeconds(2);
-            zoomIn.AutoReverse = true;
-            zoomIn.RepeatBehavior = RepeatBehavior.Forever;
-
-            //only bounce if only 42 
-            if (missingInfo && player.Age == player.DontPanic)
-            {
-                BounceEase bounceEase = new BounceEase();
-                bounceEase.Bounces = 1;
-                bounceEase.Bounciness = 4;
-                bounceEase.EasingMode = EasingMode.EaseOut;
-
-                zoomIn.EasingFunction = bounceEase;
             }
 
-            //apply the animation (and bounce if applicable) to zoomTransform
-            zoomTransform.BeginAnimation(ScaleTransform.ScaleXProperty, zoomIn);
-            zoomTransform.BeginAnimation(ScaleTransform.ScaleYProperty, zoomIn);
-        }
-
-        private void StartImageSpin()
-        {
-            DoubleAnimation rotateAnimation = new DoubleAnimation();
-
-            rotateAnimation.From = 0;
-            rotateAnimation.To = 360;
-            rotateAnimation.Duration = TimeSpan.FromSeconds(10);
-            rotateAnimation.RepeatBehavior = RepeatBehavior.Forever;
-
-            //apply the animation (spin) to rotateTransform
-            rotateTransform.BeginAnimation(RotateTransform.AngleProperty, rotateAnimation);
-        }
-
-        private void InitImageControl()
-        {
-            //only one animation can be active so we have to add both animations to a TransformGroup
-            //and then add the TransformGroup to the image RenderTransform
-            image.RenderTransformOrigin = new Point(0.5, 0.5);
-            transformGroup.Children.Add(zoomTransform);
-            transformGroup.Children.Add(rotateTransform);
-            image.RenderTransform = transformGroup;
-        }
-
         private void btnOK_Click(object sender, RoutedEventArgs e)
-        {
+            {
             bool missingInfo = false;
 
             image.Visibility = Visibility.Hidden;
@@ -167,81 +79,81 @@ namespace HHG_WPF_Fileversion
 
             //show bouncing logo, clear quotes, restore button
             if (player.Age == player.DontPanic && String.IsNullOrWhiteSpace(player.FirstName) && String.IsNullOrWhiteSpace(player.LastName))
-            {
+                {
                 missingInfo = true;
 
                 image.Visibility = Visibility.Visible;
                 tbQuote.Visibility = Visibility.Visible;
-                image.Source = player.ShowImage(missingInfo);
+                image.Source = gfxManager.ShowImage(missingInfo, player);
                 player.SetWarning(tbQuote, player);
-                FadeInImage(0.50);
-                ZoomIn(missingInfo);
+                gfxManager.FadeInImage(0.50, image);
+                gfxManager.ZoomIn(missingInfo, player);
 
                 RestoreButtonPosition();
 
                 RemoveExtraQuotes();
-            }
+                }
             else
             //show quote and logo, restore button
             if (player.Age != player.DontPanic && !String.IsNullOrWhiteSpace(player.FirstName) && !String.IsNullOrWhiteSpace(player.LastName))
-            {
+                {
                 image.Visibility = Visibility.Visible;
                 tbQuote.Visibility = Visibility.Visible;
                 player.FetchQuote(tbQuote, player);
 
-                image.Source = player.ShowImage(true);
-                FadeInImage(0.50);
-                ZoomIn(missingInfo);
+                image.Source = gfxManager.ShowImage(true, player);
+                gfxManager.FadeInImage(0.50, image);
+                gfxManager.ZoomIn(missingInfo, player);
 
                 RestoreButtonPosition();
 
                 RemoveExtraQuotes();
-            }
+                }
             else
             //show spinning hhg image, clear quotes, restore button
             if (player.Age == player.DontPanic && !String.IsNullOrWhiteSpace(player.FirstName) && !String.IsNullOrWhiteSpace(player.LastName))
-            {
+                {
                 //set song position to it's most HHG's "moment"                
-                player.audioFileReader.CurrentTime = TimeSpan.FromMinutes(1.10);
+                musicManager.audioFileReader.CurrentTime = TimeSpan.FromMinutes(1.10);
 
                 //player.fade.BeginFadeIn(1000);
-                image.Source = player.ShowImage(missingInfo);
+                image.Source = gfxManager.ShowImage(missingInfo, player);
                 image.Visibility = Visibility.Visible;
 
                 tbQuote.Visibility = Visibility.Hidden;
 
-                FadeInImage(1.0);
-                ZoomIn(missingInfo);
-                StartImageSpin();
+                gfxManager.FadeInImage(1.0, image);
+                gfxManager.ZoomIn(missingInfo, player);
+                gfxManager.StartImageSpin();
 
                 RestoreButtonPosition();
 
                 RemoveExtraQuotes();
-            }
+                }
             else
             //show chaos if any fields are empty
-            {
+                {
                 tbQuote.Visibility = Visibility.Hidden;
                 RandomizeButton();
                 MultiplyVogonQuote();
-            }
-        }//end of btnOk_Click
+                }
+            }//end of btnOk_Click
 
 
         private void RandomizeButton()
-        {
+            {
             //randomize button placement or technically the Canvas
             Canvas.SetLeft(btnOK, random.Next((int)this.Width - 100));
             Canvas.SetTop(btnOK, random.Next((int)this.Height - 100));
 
             //note: Canvas.ZIndex="1" in MainWindow.xaml makes sure the button is "on top" so it's reachable with mouse too
             //since no other elements use ZIndex we can just use 1
-        }
+            }
         private void MultiplyVogonQuote()
-        {
+            {
             //instantiate textblocks
             for (int i = 0; i <= player.DontPanic; i++)
-            {
+                {
                 textBlock = new TextBlock();
 
                 //brush must be something light since the background is pretty dark
@@ -256,33 +168,45 @@ namespace HHG_WPF_Fileversion
                 //add textBlock to MainCanvas
                 MainCanvas.Children.Add(textBlock);
 
-                //randomize textblocks
+                //randomize position of textblocks
                 Canvas.SetLeft(textBlock, random.Next((int)this.Width - 100));
                 Canvas.SetTop(textBlock, random.Next((int)this.Height - 100));
+
+                //declare and instantiate a RotateTransform
+                RotateTransform angle = new RotateTransform();
+
+                //set angle randomly
+                angle.Angle = random.Next(360);
+
+                //set origin/rotation position to center of textBlock
+                textBlock.RenderTransformOrigin = new Point(0.5, 0.5);
+
+                //attach angle to textBlocks RenderTransform
+                textBlock.RenderTransform = angle;
+                }
             }
-        }
 
         private void RemoveExtraQuotes()
-        {
+            {
             //collect textblocks into a temporary list (to avoid modifying the collection during iteration)
             List<TextBlock> textBlocksToRemove = new List<TextBlock>();
 
             //iterate through each element in canvas
             foreach (UIElement element in MainCanvas.Children)
-            {
+                {
                 //add to list if element is of type textblock AND it's name is anything but the original textblock
                 if (element is TextBlock textBlock && textBlock.Name != tbQuote.Name)
-                {
+                    {
                     textBlocksToRemove.Add(textBlock);
+                    }
                 }
-            }
 
             //now remove them
             foreach (TextBlock textBlock in textBlocksToRemove)
-            {
+                {
                 MainCanvas.Children.Remove(textBlock);
+                }
             }
-        }
 
         //private void CreateButtons()
         //    {
@@ -320,34 +244,34 @@ namespace HHG_WPF_Fileversion
         //    }
 
         private void RestoreButtonPosition()
-        {
+            {
             //restore button's original position (from MainWindow.xaml)
             Canvas.SetLeft(btnOK, 165);
             Canvas.SetTop(btnOK, 230);
-        }
+            }
 
 
         private void MainWindow1_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
-        {
+            {
             //randomize position
             //btnOK.Margin = new Thickness(random.Next(width - 100), random.Next(height), 0, 0);
 
             //attach button to mouse
             //Point position = Mouse.GetPosition(this);
             //btnOK.Margin = new Thickness(position.X, position.Y, 0, 0);
-        }
+            }
 
 
         private void MainWindow1_Unloaded(object sender, RoutedEventArgs e)
-        {
+            {
             //release and free NAudio resources
-            player.outputDevice.Stop();
-            player.outputDevice.Dispose();
-            player.audioFileReader.Dispose();
+            musicManager.outputDevice.Stop();
+            musicManager.outputDevice.Dispose();
+            musicManager.audioFileReader.Dispose();
 
             //just in case the user decides to just close the program
             //RemoveExtraButtons();
             RemoveExtraQuotes();
+            }
         }
     }
-}
